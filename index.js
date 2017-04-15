e5f79987d8dc8cf8a5d17544ce466fc5b984cbc6
Dylan Shadbolt <jaemin1904@gmail.com>
2017-04-15 23:07:10 +1000

    something

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'MzAyNDM4NTU1NjU2MTI2NDY1.C9NgMg.qMbLxg4DlczEuOukDvZ-m-6s0BQ';
-const http = require('http');
-const fs = require('fs');
-
-var server = http.createServer(function(req, res){
-    fs.readFile('index.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
-    });
-}).listen(8000);
-
-console.log("Server running at http://127.0.0.1:8000/");
 
 bot.on('ready', () => {
   console.log('I am ready!');
@@ -22,30 +10,9 @@ bot.on('message', message => {
   if (message.content === 'ping') {
     message.channel.sendMessage('pong');
   }
-  else if (message.content === '') {
-
+  else if (message.content === "I'm Bad") {
+    message.channel.sendMessage("Hello Bad, I'm Bot!")
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
 bot.login(token);
