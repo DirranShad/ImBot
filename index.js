aeb7bbc9a0743618bc129799e92b667acdb18bcf
Dylan Shadbolt <jaemin1904@gmail.com>
2017-04-15 23:03:39 +1000

    Something

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'MzAyNDM4NTU1NjU2MTI2NDY1.C9NgMg.qMbLxg4DlczEuOukDvZ-m-6s0BQ';
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-server.listen(8000);
+var server = http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
 
 console.log("Server running at http://127.0.0.1:8000/");
 
