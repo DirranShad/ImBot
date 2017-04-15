32eb957162e4c3ce0ac4d0f67ee9c3d17dd769a2
Dylan Shadbolt <jaemin1904@gmail.com>
2017-04-15 22:37:10 +1000

    Heroku Test

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'MzAyNDM4NTU1NjU2MTI2NDY1.C9NgMg.qMbLxg4DlczEuOukDvZ-m-6s0BQ';
+const http = require('http');
+
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
 
 bot.on('ready', () => {
   console.log('I am ready!');
@@ -10,6 +20,30 @@ bot.on('message', message => {
   if (message.content === 'ping') {
     message.channel.sendMessage('pong');
   }
+  else if (message.content === '') {
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 bot.login(token);
